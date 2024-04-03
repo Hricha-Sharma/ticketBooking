6900b86c13a4b10961df76cb88b83e2875e01597
Vishesh Verma <visheshverma250703@gmail.com>
2024-04-03 18:26:52 +0530

    Bugs fixed

--- app.js
+++ app.js
@@ -3,38 +3,7 @@ let url = window.location.href;
 let url_segment = url.split('?');
 // console.log(url_segment[1]);
 
-document.addEventListener('DOMContentLoaded', () => {
-    let play_btn = document.querySelector('.play');
-    let video = document.querySelector('.video');
-  
-    if (play_btn) {
-      play_btn.addEventListener('click', () => {
-        if (video.paused) {
-          video.play();
-          video.style.display = 'unset';
-          play_btn.classList.remove('fa-play');
-          play_btn.classList.add('fa-pause');
-        } else {
-          video.pause();
-          video.style.display = 'none';
-          play_btn.classList.add('fa-play');
-          play_btn.classList.remove('fa-pause');
-        }
-      });
-    } 
-    else 
-    {
-      console.error('play_btn is null or undefined');
-    }
-  
-    if (video) {
-      video.addEventListener('ended', ()=>{
-        video.play();
-      });
-    } else {
-      console.error('video is null or undefined');
-    }
-  });
+
 
 let date = new Date();
 let main_date = date.getDate();
@@ -66,7 +35,7 @@ let pvr = [
         b: [8, 5],
         a: [],
         price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 12,
+        date: 1,
         img: 'img/jawan.jpg',
         video: 'video/Jawan.mp4'
     },
@@ -88,9 +57,9 @@ let pvr = [
         b: [8, 5, 12, 13, 14],
         a: [],
         price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 12,
+        date: 1,
         img: 'img/Gadar2.jpg',
-        // video: 'video.Jawan.mp4'
+        video: 'video/Gadar2.mp4'
     },
     {
         pvr: 'PVR Vegus',
@@ -110,7 +79,7 @@ let pvr = [
         b: [8, 5],
         a: [],
         price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 13,
+        date: 2,
         img: 'img/jawan.jpg',
         video: 'video/Jawan.mp4'
     },
@@ -132,10 +101,143 @@ let pvr = [
         b: [8, 5, 12, 13, 14],
         a: [],
         price: [800, 800, 560, 560, 560, 560, 430, 430],
-        date: 13,
+        date: 2,
         img: 'img/Gadar2.jpg',
-        // video: 'video.Jawan.mp4'
+        video: 'video/Gadar2.mp4'
     },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Jawan',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 1,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [2, 6, 24, 23, 7, 16, 17, 18, 19, 13, 12],
+        h: [1, 2, 78, 20, 23, 8, 11, 18, 19, 13, 12],
+        f: [5, 6, 15, 17, 18],
+        e: [2, 7, 8, 17, 18],
+        d: [5, 16, 15, 23, 22],
+        c: [1, 2, 11, 12, 19],
+        b: [8, 5],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 3,
+        img: 'img/jawan.jpg',
+        video: 'video/Jawan.mp4'
+    },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Gadar2',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 2,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [3, 8, 21, 22, 9, 13, 16, 18, 20, 12, 15],
+        h: [5, 6, 8, 19, 24, 8, 13, 19, 21, 12, 11],
+        f: [1, 3, 13, 15, 22],
+        e: [3, 9, 10, 20, 24],
+        d: [7, 12, 9, 21, 20],
+        c: [6, 8, 11, 12, 19],
+        b: [8, 5, 12, 13, 14],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 3,
+        img: 'img/Gadar2.jpg',
+        video: 'video/Gadar2.mp4'
+    },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Jawan',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 1,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [2, 6, 24, 23, 7, 16, 17, 18, 19, 13, 12],
+        h: [1, 2, 78, 20, 23, 8, 11, 18, 19, 13, 12],
+        f: [5, 6, 15, 17, 18],
+        e: [2, 7, 8, 17, 18],
+        d: [5, 16, 15, 23, 22],
+        c: [1, 2, 11, 12, 19],
+        b: [8, 5],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 4,
+        img: 'img/jawan.jpg',
+        video: 'video/Jawan.mp4'
+    },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Gadar2',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 2,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [3, 8, 21, 22, 9, 13, 16, 18, 20, 12, 15],
+        h: [5, 6, 8, 19, 24, 8, 13, 19, 21, 12, 11],
+        f: [1, 3, 13, 15, 22],
+        e: [3, 9, 10, 20, 24],
+        d: [7, 12, 9, 21, 20],
+        c: [6, 8, 11, 12, 19],
+        b: [8, 5, 12, 13, 14],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 4,
+        img: 'img/Gadar2.jpg',
+        video: 'video/Gadar2.mp4'
+    },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Jawan',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 1,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [2, 6, 24, 23, 7, 16, 17, 18, 19, 13, 12],
+        h: [1, 2, 78, 20, 23, 8, 11, 18, 19, 13, 12],
+        f: [5, 6, 15, 17, 18],
+        e: [2, 7, 8, 17, 18],
+        d: [5, 16, 15, 23, 22],
+        c: [1, 2, 11, 12, 19],
+        b: [8, 5],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 5,
+        img: 'img/jawan.jpg',
+        video: 'video/Jawan.mp4'
+    },
+    {
+        pvr: 'PVR Vegus',
+        movie: 'Gadar2',
+        loc: 'Dwarka Sector 14, New Delhi',
+        audi: 2,
+        type: '4DX',
+        series: ['J', 'H', 'F', 'E', 'D', 'C', 'B', 'A'],
+        row_section: 3,
+        seat: 24,
+        j: [3, 8, 21, 22, 9, 13, 16, 18, 20, 12, 15],
+        h: [5, 6, 8, 19, 24, 8, 13, 19, 21, 12, 11],
+        f: [1, 3, 13, 15, 22],
+        e: [3, 9, 10, 20, 24],
+        d: [7, 12, 9, 21, 20],
+        c: [6, 8, 11, 12, 19],
+        b: [8, 5, 12, 13, 14],
+        a: [],
+        price: [800, 800, 560, 560, 560, 560, 430, 430],
+        date: 5,
+        img: 'img/Gadar2.jpg',
+        video: 'video/Gadar2.mp4'
+    },
+   
 ]
 
 
@@ -221,12 +323,50 @@ let addSeats = (arr) => {
 }
 
 let data = pvr.filter(obj => obj.date == main_date && obj.movie == url_segment[1]);
-// console.log(data);
+
+console.log(data);
+console.log(main_date);
+console.log(main_date);
 
 
 document.getElementById('title').innerText = data[0].movie;
 document.getElementById('poster').src = data[0].img;
-// document.getElementById('video').src = data[0].video;
+document.getElementsByClassName('video').src = data[0].video;
+
+document.addEventListener('DOMContentLoaded', () => {
+    let play_btn = document.querySelector('.play');
+    let video = document.querySelector('.video');
+    video.src = data[0].video
+    // console.log(video)
+  
+    if (play_btn) {
+      play_btn.addEventListener('click', () => {
+        if (video.paused) {
+          video.play();
+          video.style.display = 'unset';
+          play_btn.classList.remove('fa-play');
+          play_btn.classList.add('fa-pause');
+        } else {
+          video.pause();
+          video.style.display = 'none';
+          play_btn.classList.add('fa-play');
+          play_btn.classList.remove('fa-pause');
+        }
+      });
+    } 
+    else 
+    {
+      console.error('play_btn is null or undefined');
+    }
+  
+    if (video) {
+      video.addEventListener('ended', ()=>{
+        video.play();
+      });
+    } else {
+      console.error('video is null or undefined');
+    }
+  });
 
 
 addSeats(data)
